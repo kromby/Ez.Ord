12457dc495be2c1351f4be9f8e53ba392639cd51
Guðjón Karl Arnarson <kromby@hress.org>
2024-07-20 08:44:31 +0000

    Removing RUST importer

--- word-importer/target/debug/deps/word_importer-64541395e535a1a2.d
+++
@@ -1,5 +0,0 @@
-/home/user/Ez.Ord/word-importer/target/debug/deps/word_importer-64541395e535a1a2: src/main.rs
-
-/home/user/Ez.Ord/word-importer/target/debug/deps/word_importer-64541395e535a1a2.d: src/main.rs
-
-src/main.rs:
--- word-importer/target/debug/word-importer.d
+++
@@ -1,1 +0,0 @@
-/home/user/Ez.Ord/word-importer/target/debug/word-importer: /home/user/Ez.Ord/word-importer/src/main.rs
